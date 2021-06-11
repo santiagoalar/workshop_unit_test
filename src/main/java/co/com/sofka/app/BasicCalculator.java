22fae0f0c62ac23296c948b8388aa974f87140d4
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-11 14:38:15 -0500

    Add dependencies and properties, Create BasicCalculator

---
+++ src/main/java/co/com/sofka/app/BasicCalculator.java
@@ -0,0 +1,29 @@
+package co.com.sofka.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long sub(Long number1, Long number2) {
+        logger.info( "Subtracting {} - {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long mult(Long number1, Long number2) {
+        logger.info( "Multiplying {} * {}", number1, number2 );
+        return number1 + number2;
+    }
+
+    public Long div(Long number1, Long number2) {
+        logger.info( "Dividing {} / {}", number1, number2 );
+        return number1 + number2;
+    }
+}
